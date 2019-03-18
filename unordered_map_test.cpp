5c14f3bfc1c3aaf9c7512d26eda82e2396ca9e11
OleksiiTimofieiev <contrbas@gmail.com>
2019-03-18 22:17:01 +0200

    changes

--- unordered_map_test.cpp
+++ unordered_map_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <string>
 
 using namespace std;
 
@@ -7,9 +8,19 @@ class test
 {
 	private:
 	public:
-		int param;
+		int param1;
+		int param2;
+		int param3;
+		int param4;
+		int param5;
+		int param6;
+		int param7;
+		int param8;
+		int param9;
+		int param10;
+
 		test() {};
-		test(int x) : param(x) {}
+		test(int x, int y) : param1(x), param2(y), param3(x), param4(y), param5(x), param6(y), param7(x), param8(y), param9(x), param10(y)  {}
 		~test() {};	
 };
 
@@ -18,12 +29,16 @@ int	main(void)
 
 	unordered_map<string, test> hash;
 
-	hash.insert(std::make_pair("1", test(1)));
+	for (int i = 0; i < 10000; ++i)
+	{
+		hash.insert(std::make_pair(std::to_string(i), test(i, i)));
+		/* code */
+	}
 	// hash.insert(std::make_pair("2", test(2)));
 	// hash.insert(std::make_pair("3", test(3)));
 
-	std::for_each(hash.begin(), hash.end() , [](std::pair<std::string, test > element){
-				std::cout<<element.first << " :: "<<element.second.param<<std::endl;
+	std::for_each(hash.begin(), hash.end() , [](std::pair<std::string, test> element){
+				std::cout << element.first << " :: "<< element.second.param1 << std::endl;
 				});
 
 
