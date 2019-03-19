08158ab4967dcca008eb8a10f0d6ba08523024bb
contrbas@gmail.com <K4rmU78B>
2019-03-19 10:47:02 +0200

    working on regex

--- source/main.cpp
+++ source/main.cpp
@@ -4,7 +4,7 @@
 
 void	regex_check(std::string str, int *not_ok) 
 {
-	const std::regex regex_rule("\\{ (CA|SW|RT) (Ports:[0-9]+) (SystemGUID:[A-Za-z0-9]+) (NodeGUID:[A-Za-z0-9]+) (PortGUID:[A-Za-z0-9]+) (VenID:[A-Za-z0-9]+) (DevID:[A-Za-z0-9]+) (Rev:[A-Za-z0-9]+) (\\{.+?\\}) (LID:[A-Za-z0-9]+) (PN:[A-Za-z0-9]+) \\} \\{ (CA|SW|RT) (Ports:[0-9]+) (SystemGUID:[A-Za-z0-9]+) (NodeGUID:[A-Za-z0-9]+) (PortGUID:[A-Za-z0-9]+) (VenID:[A-Za-z0-9]+) (DevID:[A-Za-z0-9]+) (Rev:[A-Za-z0-9]+) (\\{.+?\\}) (LID:[A-Za-z0-9]+) (PN:[A-Za-z0-9]+) \\} (PHY=(1x|2x|4x)) (LOG=(DWN|INIT|ARM|ACT)) (SPD=(5|10|20|14|25))");
+	const std::regex regex_rule("^\\{ (CA|SW|RT) (Ports:[0-9]+) (SystemGUID:[A-Za-z0-9]+) (NodeGUID:[A-Za-z0-9]+) (PortGUID:[A-Za-z0-9]+) (VenID:[A-Za-z0-9]+) (DevID:[A-Za-z0-9]+) (Rev:[A-Za-z0-9]+) (\\{.+?\\}) (LID:[A-Za-z0-9]+) (PN:[A-Za-z0-9]+) \\} \\{ (CA|SW|RT) (Ports:[0-9]+) (SystemGUID:[A-Za-z0-9]+) (NodeGUID:[A-Za-z0-9]+) (PortGUID:[A-Za-z0-9]+) (VenID:[A-Za-z0-9]+) (DevID:[A-Za-z0-9]+) (Rev:[A-Za-z0-9]+) (\\{.+?\\}) (LID:[A-Za-z0-9]+) (PN:[A-Za-z0-9]+) \\} (PHY=(1x|2x|4x)) (LOG=(DWN|INIT|ARM|ACT)) (SPD=(5|10|20|14|25))$");
 
 	std::smatch result;
 
@@ -14,12 +14,14 @@ void	regex_check(std::string str, int *not_ok)
 		// std::cout << "Whole match : " << result.str(0) << std::endl;
 		// std::cout << "First capturing group is '" << result.str(1) << "' which is captured at index " << result.position(1) << std::endl;
 		// std::cout << "Second capturing group is '" << result.str(2) << "' which is captured at index " << result.position(2) << std::endl;
-		std::cout << "------ cool ------" << std::endl;
+		// std::cout << "------ cool ------" << std::endl;
 	}
 	else
 	{
 		(*not_ok)++;
-		std::cout << "****** not cool ******" << std::endl;
+		int tmp = (*not_ok) - 1;
+		std::cout << str << std::endl;
+		std::cout << "line -> " << tmp << "****** not cool ******" << std::endl;
 	}
 
 
@@ -28,7 +30,7 @@ void	regex_check(std::string str, int *not_ok)
 
 int		main(void)
 {
-	std::string		input = "";
+	std::string		input;
 	std::string 	path = "./test_file/opensm-subnet.lst";
 	std::ifstream 	fin;
 
@@ -51,7 +53,7 @@ int		main(void)
 			getline(fin, input);
 			line_number++;
 
-			std::cout << line_number << " -> ";
+			// std::cout << line_number << " -> ";
 			regex_check(input, &not_ok);
 		}
 	}
