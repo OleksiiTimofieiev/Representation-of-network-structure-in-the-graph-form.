0ec88b5d05a03751af5397fca50ad560329f956d
contrbas@gmail.com <K4rmU78B>
2019-03-19 11:44:49 +0200

    smatch iteration

--- source/main.cpp
+++ source/main.cpp
@@ -2,14 +2,25 @@
 
 #include "../includes/lexer.hpp"
 
+// TODO: operand class;
 // TODO: parser ;
 // TODO: change the name of the binary file ;
 // TODO: check for leaks;
 // TODO: delete debug from lexer ;
+// TODO: explain error manager;
+// TODO: explain data structure selection;
+
+// TODO: handle errors with input;
+
+// space complexity: o(v) + o(e);
+// unordered_map;
+// try catch for data types;
+// check for overlap (bigge value)
+// obligatory prameters;
 
 int		main(void)
 {
-	Lexer lexer;
+	Lexer lexer; /* class to parse and validate input */
 
 	std::string		input;
 	std::string 	path = "./test_file/opensm-subnet.lst";
@@ -33,13 +44,15 @@ int		main(void)
 		while (!(fin.eof()))
 		{
 			getline(fin, input);
+
 			line_number++;
 
-			// std::cout << line_number << " -> ";
 			lexer.regex_check(input, &not_ok, line_number);
 		}
 	}
-			std::cout << "total not valid lines -> " << not_ok << std::endl;
+
+	std::cout << "total not valid lines -> " << not_ok << std::endl;
+
 	fin.close();
 
 	return (0);
