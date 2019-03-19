7244463c6e27fadb4d50ac88dc0c0524454bf135
contrbas@gmail.com <K4rmU78B>
2019-03-19 17:08:46 +0200

    started working on visualization

--- includes/core.hpp
+++ includes/core.hpp
@@ -9,27 +9,25 @@
 
 class Core
 {
-private:
-	Lexer		lexer; /* class to validate input */
-	Parser		parser; /* class to push input to memory */
-	Database	database; /* class to keep the information */
-	Visualizer	visualizer; /* class for the data visualization */
+	private:
+		Lexer						lexer; /* class to validate input */
+		Parser						parser; /* class to push input to memory */
+		Database					database; /* class to keep the information */
+		Visualizer					visualizer; /* class for the data visualization */
 
-	std::ifstream fin; /* file descriptor to the input file */
-	std::string input; /* container for the line read */
-	std::string path;
+		std::ifstream 				fin; /* file descriptor to the input file */
+		std::string 				input; /* container for the line read */
+		std::string 				path; /* path to the file for reading purposes */
 
-	std::pair<Vertex, Vertex> line; /* buffer container for the line read */
+		std::pair<Vertex, Vertex>	line; /* buffer container for the line read */
 
-	int line_number = 0;
-	int not_ok = 0;
-public:
-	Core(const std::string path);
-	~Core();
-	void	main_cycle(void);
+		int line_number = 0;
+		int not_ok = 0;
+
+	public:
+		Core(const std::string path);
+		~Core();
+		void	main_cycle(void);
 };
-
-
-
 
 #endif
