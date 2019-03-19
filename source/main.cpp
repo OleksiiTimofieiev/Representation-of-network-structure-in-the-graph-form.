b60bd4ba1a1a0d5a833953c2b2a06d1ff78fa96a
contrbas@gmail.com <K4rmU78B>
2019-03-19 09:25:49 +0200

    started working on regex

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,33 @@
 #include <fstream>
 #include <regex>
 
+void	regex_check(std::string str) 
+{
+	const std::regex regex_rule("\\{ (CA) (Ports:01) (SystemGUID:7cfe9003004b0700) (NodeGUID:7cfe9003004b0700) (PortGUID:7cfe9003004b0700) (VenID:0002C9) (DevID:1013) (Rev:00000000) (\\{ime5 HCA-1\\}) (LID:0032) (PN:01) \\} \\{ (SW) (Ports:25) (SystemGUID:7cfe900300992080) (NodeGUID:7cfe900300f2da60) (PortGUID:7cfe900300f2da60) (VenID:000002C9) (DevID:CF080000) (Rev:000000A0) (\\{MF0;cs7500:CS7500/L17/U2\\}) (LID:00F9) (PN:0C) \\} (PHY=4x) (LOG=ACT) (SPD=25)");
+
+	std::smatch result;
+
+	if (std::regex_match(str, result, regex_rule))
+	{
+		std::cout << "------ cool ------" << std::endl;
+
+		std::cout << "Whole match : " << result.str(0) << std::endl;
+		std::cout << "First capturing group is '" << result.str(1)
+			 << "' which is captured at index " << result.position(1)
+			 << std::endl;
+		std::cout << "Second capturing group is '" << result.str(2)
+			 << "' which is captured at index " << result.position(2)
+			 << std::endl;
+	}
+	else
+	{
+		std::cout << "****** not cool ******" << std::endl;
+	}
+
+
+	
+}
+
 int		main(void)
 {
 	std::string		input = "";
@@ -20,6 +47,8 @@ int		main(void)
 		while (!(fin.eof()))
 		{
 			getline(fin, input);
+
+			regex_check(input);
 			std::cout << input << std::endl;
 		}
 	}
