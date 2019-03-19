9b34556d95b5eefc30d0c501f30a6ad4b03fd67f
contrbas@gmail.com <K4rmU78B>
2019-03-19 18:23:25 +0200

    show node

--- source/visualizer.cpp
+++ source/visualizer.cpp
@@ -13,8 +13,8 @@ void Visualizer::show_nodes_all(const Database &database) const
 
 void Visualizer::show_node(const Database & database, const std::string str) const 
 {
+	auto hash = database.hash;
 
-
-	std::cout << database.hash[str] << std::endl;
+	std::cout << hash[str] << std::endl;
 
 };
