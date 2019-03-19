fe437cd005caedd4bab1d76b100c04ab22bcb861
contrbas@gmail.com <K4rmU78B>
2019-03-19 10:06:58 +0200

    working on regex

--- source/main.cpp
+++ source/main.cpp
@@ -4,16 +4,16 @@
 
 void	regex_check(std::string str) 
 {
-	const std::regex regex_rule("\\{ (CA) (Ports:01) (SystemGUID:7cfe9003004b0700) (NodeGUID:7cfe9003004b0700) (PortGUID:7cfe9003004b0700) (VenID:0002C9) (DevID:1013) (Rev:00000000) (\\{ime5 HCA-1\\}) (LID:0032) (PN:01) \\} \\{ (SW) (Ports:25) (SystemGUID:7cfe900300992080) (NodeGUID:7cfe900300f2da60) (PortGUID:7cfe900300f2da60) (VenID:000002C9) (DevID:CF080000) (Rev:000000A0) (\\{MF0;cs7500:CS7500/L17/U2\\}) (LID:00F9) (PN:0C) \\} (PHY=4x) (LOG=ACT) (SPD=25)");
+	const std::regex regex_rule("\\{ (CA|SW) (Ports:[0-9]+) (SystemGUID:[A-Za-z0-9]+) (NodeGUID:[A-Za-z0-9]+) (PortGUID:[A-Za-z0-9]+) (VenID:[A-Za-z0-9]+) (DevID:[A-Za-z0-9]+) (Rev:[A-Za-z0-9]+) (\\{ime5 HCA-1\\}) (LID:[A-Za-z0-9]+) (PN:01) \\} \\{ (SW) (Ports:25) (SystemGUID:7cfe900300992080) (NodeGUID:7cfe900300f2da60) (PortGUID:7cfe900300f2da60) (VenID:000002C9) (DevID:CF080000) (Rev:000000A0) (\\{MF0;cs7500:CS7500/L17/U2\\}) (LID:00F9) (PN:0C) \\} (PHY=4x) (LOG=ACT) (SPD=25)");
 
 	std::smatch result;
 
 	if (std::regex_match(str, result, regex_rule))
 	{
 
-		std::cout << "Whole match : " << result.str(0) << std::endl;
-		std::cout << "First capturing group is '" << result.str(1) << "' which is captured at index " << result.position(1) << std::endl;
-		std::cout << "Second capturing group is '" << result.str(2) << "' which is captured at index " << result.position(2) << std::endl;
+		// std::cout << "Whole match : " << result.str(0) << std::endl;
+		// std::cout << "First capturing group is '" << result.str(1) << "' which is captured at index " << result.position(1) << std::endl;
+		// std::cout << "Second capturing group is '" << result.str(2) << "' which is captured at index " << result.position(2) << std::endl;
 		std::cout << "------ cool ------" << std::endl;
 	}
 	else
@@ -33,8 +33,8 @@ int		main(void)
 
 	int line_number = 0;
 
-	int ok = 0;
-	int not_ok = 0;
+	// int ok = 0;
+	// int not_ok = 0;
 
 	fin.open(path);
 
