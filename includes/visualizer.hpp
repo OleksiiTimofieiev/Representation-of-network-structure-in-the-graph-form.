8d8c4a901bde486f6ae618e61f56e62af910dd5e
contrbas@gmail.com <K4rmU78B>
2019-03-19 18:26:33 +0200

    show node

--- includes/visualizer.hpp
+++ includes/visualizer.hpp
@@ -13,6 +13,7 @@ public:
 
 	void	show_nodes_all(const Database & database) const;
 	void 	show_node(const Database &database, const std::string str) const;
+	void	show_node_neighbors(const Database &database, const std::string str) const;
 };
 
 #endif
--- source/visualizer.cpp
+++ source/visualizer.cpp
@@ -4,17 +4,27 @@ Visualizer::Visualizer() {}
 
 Visualizer::~Visualizer() {}
 
-void Visualizer::show_nodes_all(const Database &database) const
+void	Visualizer::show_nodes_all(const Database &database) const
 {
 	std::for_each(database.hash.begin(), database.hash.end(), [](std::pair<std::string, Vertex> element) {
 		std::cout << element.second << std::endl;
 	});
 };
 
-void Visualizer::show_node(const Database & database, const std::string str) const 
+void	Visualizer::show_node(const Database & database, const std::string str) const 
 {
 	auto hash = database.hash;
 
 	std::cout << hash[str] << std::endl;
 
 };
+
+void	Visualizer::show_node_neighbors(const Database & database, const std::string str) const
+{
+	auto hash = database.hash;
+
+	for (auto data : hash[str].edges)
+	{
+		std::cout << data << std::endl;
+	}
+}
